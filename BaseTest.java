a9248212eb26705837bba022ad20e73c126cf260
CH AAMAN KUMAR <161840066+AKJOK@users.noreply.github.com>
2024-03-26 15:01:44 +0530

    Add files via upload

---
+++ BaseTest.java
@@ -0,0 +1,33 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class BaseTest {
+	
+	public static WebDriver driver;
+
+	String url = "https://www.fb.com";//from dev1 & from dev2
+	
+	@BeforeTest
+	public void setup() {
+		//open the browser here
+
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");
+		driver = new ChromeDriver(options);//opens the chrome browser
+		
+		//open the url
+		driver.get(url);
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		//close the browser
+		driver.quit();
+	}
+
+}
---
+++ BasicValidation.java
@@ -0,0 +1,27 @@
+package test;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class BasicValidation extends BaseTest{
+
+	@Test
+	public void urlValidation() {
+		String expectedResult = "https://www.facebook.com/";
+		String actualResult = driver.getCurrentUrl();
+		
+		//display
+		Reporter.log("expected result = " + expectedResult);
+		Reporter.log("actual result = " + actualResult);
+		
+		//comparision
+		assertTrue(actualResult.equals(expectedResult), "Mismatch in the url");
+		//some code changes
+		//cc from b1 branch
+		//demo for stash- dev2 cc
+		//from s1 branch
+
+	}
+}
